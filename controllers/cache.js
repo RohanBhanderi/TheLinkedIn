301fc1fa2ff6f2b2dd87f09c30d086bb54bc9024
Sambu Gopan <sambu.9589@gmail.com>
2015-04-12 10:13:26 -0700

    Create cache.js

---
+++ controllers/cache.js
@@ -0,0 +1,45 @@
+/**
+ * New node file
+ */
+var redis = require('redis');
+var client = redis.createClient();
+var data=[{
+	"name":"sambu",
+	"lname":"gopan"
+},
+{
+	"name":"sam",
+	"lname":"gop"
+}
+];
+
+redistrial=function(res,req){
+
+client.on('connect', function(err,reply) {
+	console.log('3');
+	if(err){
+		console.log('cache is not available');
+		}else{
+    console.log('connected');
+		}
+});
+
+client.set('framework1', JSON.stringify(data),function(err,reply){
+	console.log('1');
+	client.get('framework1',function(err,reply){
+		console.log('2');
+		if (reply == null) {
+			console.log('here');
+	       req.end('/cache');
+	    } else {
+	    	console.log('there');
+	    	jsonobj=JSON.parse(reply);
+	        req.end(JSON.stringify(jsonobj));
+	    }
+	});
+	console.log('dissapointed');
+});
+
+};
+
+exports.redistrial=redistrial;
