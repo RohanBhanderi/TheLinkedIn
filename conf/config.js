d47b65c4f2b5187ad41b0425a1d145b14b64cfd8
Rohan Bhanderi <rohan.bhanderi@gmail.com>
2019-10-19 18:30:03 -0700

    Remove AWS access key and secret keys (#4)

--- conf/config.js
+++ conf/config.js
@@ -2,49 +2,48 @@
  * Database Configurations
  */
 exports.db = {
-
-	/*	// Local
-		 "host" : "localhost",
-		 "port" : 3306,
-		 "user" : "root",
-		 "password" : "",
-		 "database" : "cmpe_282",
-		*/
-	
-		//AWS
-		"host" : "cmpe282rds.csycvfjjat3i.us-west-1.rds.amazonaws.com",
+    // Local
+	/*
+        "host" : "localhost",
         "port" : 3306,
-		"user" : "cmpe282RDS",
-		"password" : "cmpe282RDS",
-		"database" : "CMPE_282"		 
+        "user" : "root",
+        "password" : "",
+        "database" : "cmpe_282",
+    */
+
+    //AWS
+    "host" : "",
+    "port" : 3306,
+    "user" : "cmpe282RDS",
+    "password" : "cmpe282RDS",
+    "database" : "CMPE_282"
 };
 
 /*
  * Database Pooling Configurations
 */
 exports.dbPool = {
-		"maxSize" : 50
+    "maxSize" : 50
 };
 
 
 // AWS configurations
-
 exports.awsConfig = {
-	accessKeyId: 'AKIAJ3FYGQLIURBVOH4Q',
-	secretAccessKey: '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',
-	region: 'us-west-1'
+	accessKeyId: '',
+	secretAccessKey: '',
+	region: ''
 };
 
 
-/* Redis config
- 
+// Redis config
 exports.redisConfig = {
-	host : 'replica282-001.halkdi.0001.usw2.cache.amazonaws.com',
-	port : '6379'
-}*/
+	host : '',
+	port : ''
+}
 
+// MongoDB config
 exports.mongoConfig = {
-	accessKeyId: 'AKIAJ3FYGQLIURBVOH4Q',
-	secretAccessKey: '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',
-	region: 'us-west-1'
+	accessKeyId: '',
+	secretAccessKey: '',
+	region: ''
 };
